cb93774dc3f0a4915110b889c45fa2e61823ea57
bakseungho <bakseungho@naver.com>
2024-04-14 05:47:26 +0900

    파일 업로드 및 테스트 커밋

--- Front-end/src/routes/Mypage/Mypage.jsx
+++ Front-end/src/routes/Mypage/Mypage.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from '../components/Header';
 
 export default function Mypage() {
   return (
-    <div>Mypage</div>
-  )
+    <>
+      <Header />
+
+      <div>마이페이지 영역입니다.</div>
+    </>
+  );
 }
