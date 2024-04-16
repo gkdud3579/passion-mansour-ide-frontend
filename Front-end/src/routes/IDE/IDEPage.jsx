a7b384b750093da425579923b355842729271554
Hayoung Shim <gkdud3579@gmail.com>
2024-04-16 17:39:15 +0900

    Design: 채팅버튼 클릭시, 채팅창UI 구현 #2

--- Front-end/src/routes/IDE/IDEPage.jsx
+++ Front-end/src/routes/IDE/IDEPage.jsx
@@ -1,20 +1,25 @@
-import React from "react";
-import Editor from "./components/Editor";
-// import Sidebar from "./components/Sidebar";
-import Toolbar from "./components/Toolbar";
-import Output from "./components/Output";
-import { ChakraProvider } from "@chakra-ui/react";
-import styles from "./IDEPage.module.css";
+import { useState } from 'react';
+import Editor from './components/Editor';
+import Toolbar from './components/Toolbar';
+import Output from './components/Output';
+import Chatting from './components/modal/Chatting';
+import { ChakraProvider } from '@chakra-ui/react';
+import styles from './IDEPage.module.css';
 
 const IDEPage = () => {
+  const [isChatVisible, setIsChatVisible] = useState(false);
+
+  const toggleChat = () => setIsChatVisible(!isChatVisible);
+
   return (
     <ChakraProvider>
       <div className={styles.page}>
-        <Toolbar />
+        <Toolbar onChatToggle={toggleChat} />
         <div className={styles.main}>
           {/* <Sidebar /> */}
           <Editor className={styles.editorContainer} />
           <Output className={styles.outputContainer} />
+          {isChatVisible && <Chatting />} {/* 채팅 모달을 조건부 렌더링으로 추가합니다 */}
         </div>
       </div>
     </ChakraProvider>
--- Front-end/src/routes/IDE/components/Toolbar.jsx
+++ Front-end/src/routes/IDE/components/Toolbar.jsx
@@ -1,6 +1,6 @@
-import styles from "./Toolbar.module.css";
+import styles from './Toolbar.module.css';
 
-const Toolbar = () => {
+const Toolbar = ({ onChatToggle }) => {
   return (
     <div className={styles.container}>
       <div className={styles.leftButtons}>
@@ -10,12 +10,10 @@ const Toolbar = () => {
           <span>JavaScript</span>
           <span>4/5</span>
         </div>
-        <span className={styles.title}>
-          백준 레벨 1 문제 1 문제 풀이합니다.
-        </span>
+        <span className={styles.title}>백준 레벨 1 문제 1 문제 풀이합니다.</span>
       </div>
       <div className={styles.rightButtons}>
-        <button>채팅</button>
+        <button onClick={onChatToggle}>채팅</button>
         <button>저장</button>
         <button>재생</button>
         <button>환경설정</button>
